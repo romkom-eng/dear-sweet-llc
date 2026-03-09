a979a9dc499885c79dc0d0b0ecb0d12601abcb9c
김지연 <kimjiyeon@gimjiyeon-ui-MacBookPro.local>
2026-03-09 13:59:58 -0700

    Auto-generate sitemap.xml for Google Search Console

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+import Sitemap from 'vite-plugin-sitemap'
+
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [
-        react()
+        react(),
+        Sitemap({
+            hostname: 'https://dearsweet.pages.dev',
+            dynamicRoutes: [
+                '/',
+                '/order',
+                '/privacy',
+                '/terms'
+            ]
+        })
     ],
 })
